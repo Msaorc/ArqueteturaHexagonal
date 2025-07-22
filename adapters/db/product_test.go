1ba2edef9eb16baf2fcc80ab26cb6c08abc3460c
marcos <msaorc@hotmail.com>
2025-07-22 09:00:01 -0300

    Implementação do teste do metodo Save

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/Msaorc/ArqueteturaHexagonal/adapters/db"
+	"github.com/Msaorc/ArqueteturaHexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -46,9 +47,33 @@ func TestProductDb_Get(t *testing.T) {
 	defer Db.Close()
 	ProductDb := db.NewProductDb(Db)
 	product, err := ProductDb.Get("abc")
-	
+
 	require.Nil(t, err)
 	require.Equal(t, "Product Test", product.GetName())
-	require.Equal(t, 0, product.GetPrice())
+	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
