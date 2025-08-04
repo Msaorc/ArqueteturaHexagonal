b59b9b11a4d456d80faeb2fee0337b13235c0c3c
marcos <msaorc@hotmail.com>
2025-08-04 08:46:09 -0300

    Criando adaptador do lado esquerdo do exagono, lado do cliente

---
+++ adapters/cli/product.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/Msaorc/ArqueteturaHexagonal/application"
+)
+
+func Run(service application.ProductServiceInterface, action string, producId string, productName string, price float32) (string, error) {
+	var result = ""
+	switch action {
+	case "create":
+		product, err := service.Create(productName, price)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product ID %s with the name %s has been created with the price %s",
+			product.GetId(), product.GetName(), product.GetPrice())
+	case "enable":
+		product, err := service.Get(producId)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Enable(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s hab been enabled.", res.GetName())
+	case "disable":
+		product, err := service.Get(producId)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Disable(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s hab been disable.", res.GetName())
+	default:
+		res, err := service.Get(producId)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product ID: %s\nName: %s\nPrice: %s\nStatus: %s",
+			res.GetId(), res.GetName(), res.GetPrice(), res.GetStatus())
+	}
+	return result, nil
+}
